0c25152b1865caea6cda9a37c27e23238d02d029
codefaztaz <codefaztaz@gmail.com>
2020-12-12 15:47:20 +0000

    2 commit
    I added Bootstrap, games component, home component, about component, navbar component, routes and some styles css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent} from './components/home/home.component';
+import { AboutComponent} from './components/about/about.component';
+import { GamesComponent} from './components/games/games.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent }, 
+  { path: 'about', component: AboutComponent },
+  { path: 'games', component: GamesComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
