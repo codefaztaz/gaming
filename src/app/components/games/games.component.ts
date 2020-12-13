100352f5fb53bba7d54a7f50ea44ea3516d50bef
codefaztaz <codefaztaz@gmail.com>
2020-12-13 19:24:43 +0000

    1 commit pagination
    I am going to create a pagination for games

--- src/app/components/games/games.component.ts
+++ src/app/components/games/games.component.ts
@@ -10,6 +10,7 @@ import { GamesService } from '../../services/games.service';
 export class GamesComponent implements OnInit {
   public gameslist: any[] = [];
   public genres;
+  public rating;
 
   constructor(
     private gamesService:GamesService,
@@ -17,14 +18,28 @@ export class GamesComponent implements OnInit {
     this.gamesService.getListGames().subscribe( (data: any) =>{
       console.log(data.results);
       this.gameslist = data.results;
+      this.rating = data.forEach(eachData =>{
+        console.log('rating', eachData.results.rating);
+      });
+      console.log(this.rating);
+
       
 
-   
-
+      console.log(data.results.rating);
     });
    }
 
   ngOnInit(): void {
+  }
+
+  listRating()
+  {
+    // get all rating values from request
+
+    // if else blocks by rating values
+
+    // change star color by if else blocks
+
   }
 
   
